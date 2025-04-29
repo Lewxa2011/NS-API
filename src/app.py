a4658028dec119514c0e73a1cd7929b6ed3916f8
Lewxa2011 <0coltonj00@gmail.com>
2025-04-29 16:25:57 -0400

    dih

--- src/app.py
+++ src/app.py
@@ -5,8 +5,10 @@ import time
 import os
 from cryptography.fernet import Fernet
 from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///accounts.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
